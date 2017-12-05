2c6ca89632aa73e02bd8738fdabc70427c8154e0
Matej Štorga <matej.storga@fer.hr>
2017-12-05 11:11:45 +0100

    prvi commit

---
+++ Interfaces/TodoItem.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+
+        public bool IsCompleted => DateCompleted.HasValue;
+
+        public DateTime? DateCompleted { get; set; }
+        public DateTime DateCreated { get; set; }
+
+        public TodoItem(string text)
+        {
+            Id = Guid.NewGuid();
+            Text = text;
+            DateCreated = DateTime.UtcNow;
+        }
+        public bool MarkAsCompleted()
+        {
+            if (!IsCompleted)
+            {
+                DateCompleted = DateTime.Now;
+                return true;
+            }
+            return false;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as TodoItem;
+            return item != null &&
+                   Id.Equals(item.Id);
+        }
+
+        public override int GetHashCode()
+        {
+            return 2108858624 + EqualityComparer<Guid>.Default.GetHashCode(Id);
+        }
+
+        /// <summary >
+        /// User id that owns this TodoItem
+        /// </ summary >
+        public Guid UserId { get; set; }
+        /// <summary >
+        /// /// List of labels associated with TodoItem
+        /// </ summary >
+        public List<TodoItemLabel> Labels { get; set; }
+        /// <summary >
+        /// Date due . If null , no date was set by the user
+        /// </ summary >
+        public DateTime? DateDue { get; set; }
+        public TodoItem(string text, Guid userId)
+        {
+            Id = Guid.NewGuid();
+            Text = text;
+            DateCreated = DateTime.UtcNow;
+            UserId = userId;
+            Labels = new List<TodoItemLabel>();
+        }
+        public TodoItem()
+        {
+            // entity framework needs this one
+            // not for use :)
+        }
+    }
+
+
+    /// <summary >
+    /// Label describing the TodoItem .
+    /// e.g. Critical , Personal , Work ...
+    /// </ summary >
+    public class TodoItemLabel
+    {
+        public Guid Id { get; set; }
+        public string Value { get; set; }
+        /// <summary >
+        /// All TodoItems that are associated with this label
+        /// </ summary >
+        public List<TodoItem> LabelTodoItems { get; set; }
+        public TodoItemLabel(string value)
+        {
+            Id = Guid.NewGuid();
+            Value = value;
+            LabelTodoItems = new List<TodoItem>();
+        }
+    }
+
+}
---
+++ Zad1/TodoSqlRepository.cs
@@ -0,0 +1,57 @@
+﻿using Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad1
+{
+    public class TodoSqlRepository : ITodoRepository
+    {
+        public void Add(TodoItem todoItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TodoItem Get(Guid todoId, Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TodoItem> GetActive(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TodoItem> GetAll(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TodoItem> GetCompleted(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TodoItem> GetFiltered(Func<TodoItem, bool> filterFunction, Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MarkAsCompleted(Guid todoId, Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(Guid todoId, Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TodoItem todoItem, Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
